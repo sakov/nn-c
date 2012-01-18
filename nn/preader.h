0854bcf333e9477ebc5eee6a581a760f9cc27c87
pavel.sakov@gmail.com <pavel.sakov@gmail.com@99152f64-3fc1-11de-b8bc-ddcf2599578a>
2012-01-18 23:29:15 +0000

    v. 1.83

--- nn/preader.h
+++ nn/preader.h
@@ -21,7 +21,7 @@ typedef struct preader preader;
 
 preader* preader_create1(double xmin, double xmax, double ymin, double ymax, int nx, int ny);
 preader* preader_create2(char* fname);
-point* preader_getpoint(preader * pr);
-void preader_destroy(preader * pr);
+point* preader_getpoint(preader* pr);
+void preader_destroy(preader* pr);
 
 #endif
